c6173ec8dab72746f90a2b82335646e275eab79c
vaishnavivs007 <vaishnavishejwadkar>
2025-04-24 11:42:07 +0530

    saved the java file

--- Demo.java
+++ Demo.java
@@ -4,5 +4,7 @@ public class Demo {
         int b - 7;
         System.out.println("Addition:" +(a+b));
         System.out.println("Subtraction:" +(a-b));
+        System.out.println("Multiplication:" +(a*b));
+        System.out.println("Division:" +(a/b));
     }
 }
