cf3038c12ab97c18a406d4a78a621940e0ce23e9
vaishnavivs007 <vaishnavishejwadkar>
2025-04-24 11:39:12 +0530

    save the java file

---
+++ Demo.java
@@ -0,0 +1,8 @@
+public class Demo {
+    public static void main(String[] args) {
+        int a = 5;
+        int b - 7;
+        System.out.println("Addition:" +(a+b));
+        System.out.println("Subtraction:" +(a-b));
+    }
+}
