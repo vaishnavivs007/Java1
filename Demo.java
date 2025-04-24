630ea05e92ab460771365f310238262b5f098874
vaishnavivs007 <vaishnavishejwadkar>
2025-04-24 12:39:57 +0530

    saved the java file

--- Demo.java
+++ Demo.java
@@ -1,10 +1,10 @@
 public class Demo {
     public static void main(String[] args) {
         int a = 5;
-        int b - 7;
-        System.out.println("Addition:" +(a+b));
-        System.out.println("Subtraction:" +(a-b));
-        System.out.println("Multiplication:" +(a*b));
-        System.out.println("Division:" +(a/b));
+        int b = 7;
+        System.out.println("Addition:" + (a + b));
+        System.out.println("Subtraction:" + (a - b));
+        System.out.println("Multiplication:" + (a * b));
+        System.out.println("Division:" + (a / b));
     }
 }
